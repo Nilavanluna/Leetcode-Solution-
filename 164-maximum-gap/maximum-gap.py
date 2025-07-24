db01b6297c8e9b2063cd09e77eb615468af397fd
Nilavanluna <103325766+Nilavanluna@users.noreply.github.com>
2025-07-24 22:07:34 +0530

    Time: 1673 ms (5.03%) | Memory: 21.6 MB (92.61%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maximumGap(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max=0
+        nums= sorted(nums)
+        print(nums)
+        if len(nums)<2:
+            return 0
+        for i  in range(1,len(nums)):
+            if nums[i]-nums[i-1]>max:
+                max=nums[i]-nums[i-1]
+                print(max)
+        return max
