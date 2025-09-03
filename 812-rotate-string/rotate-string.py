727b309ee8e031c6c8e23121abef0b132962d0a4
Nilavanluna <103325766+Nilavanluna@users.noreply.github.com>
2025-09-03 15:13:00 +0530

    Time: 4 ms (6.26%) | Memory: 12.5 MB (8.40%) - LeetSync

---
+++ 812-rotate-string/rotate-string.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        for i in range(len(s)):
+            v=s[i+1:]+s[:i+1]
+            print(v)
+            if v==goal:
+              return True
+        return False      
